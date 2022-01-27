98f20623c418ed2eb4086f9f465ee30dbf8a9935
keithdesantis <kwdesantis@wpi.edu>
2022-01-27 15:39:33 -0500

    Updated main to take in command line args

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,24 @@ public class Main {
         Coordinate start = new Coordinate(0,0);
         Coordinate goal = new Coordinate(0,0);
 
+        String filename = args[0];
+        int heuristic = 0;
+        try {
+            heuristic = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            System.out.println("Second argument must be an integer 1-6.");
+            return;
+        }
+
         try {
 
-            board = boardReader.read("board.txt", start, goal);
+            board = boardReader.read(filename, start, goal);
 
         } catch (IOException e) { System.out.println("Could not find file.");}
 
         Coordinate[][] coordBoard = boardReader.translateToCoord(board);
 
-        AStar test = AStarFactory.produceAstarWithSpecificHeuristics(4);
+        AStar test = AStarFactory.produceAstarWithSpecificHeuristics(heuristic);
         Result result = test.findPath(board, start, goal);
 
         System.out.println(result);
